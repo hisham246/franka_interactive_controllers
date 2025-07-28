b4b9e1878f0fa999a60913a921d73136a24d7e2e
hisham246 <hishamkhalil.eng@gmail.com>
2025-07-27 20:43:48 -0400

    test

--- include/franka_joint_controllers/joint_impedance_franka_controller.h
+++ include/franka_joint_controllers/joint_impedance_franka_controller.h
@@ -65,7 +65,7 @@ class JointImpedanceFrankaController : public controller_interface::MultiInterfa
   std::array<double, 7> target_q_d_;
   std::array<double, 7> dq_d_;
   std::array<double, 7> dq_;
-  double q_filt_ {0.4};  // amount off target to add to desired
+  double q_filt_ {0.5};  // amount off target to add to desired
   double epsilon_ = 0.0001;
   double max_abs_vel_ = 2.1;
 
--- src/franka_joint_controllers/joint_impedance_franka_controller.cpp
+++ src/franka_joint_controllers/joint_impedance_franka_controller.cpp
@@ -200,10 +200,10 @@ void JointImpedanceFrankaController::update(const ros::Time& /*time*/,
     dq_d_[i] = 0.0;
   }
   
-  // double alpha = 0.99;
-  // for (size_t i = 0; i < 7; i++) {
-  //   dq_filtered_[i] = (1 - alpha) * dq_filtered_[i] + alpha * dq_[i];
-  // }
+  double alpha = 0.7;
+  for (size_t i = 0; i < 7; i++) {
+    dq_filtered_[i] = (1 - alpha) * dq_filtered_[i] + alpha * dq_[i];
+  }
 
   for (size_t i = 0; i < 7; i++) {
     q_filtered_[i] = (1 - q_filt_) * q_filtered_[i] + q_filt_ * q_d_[i];
@@ -226,7 +226,7 @@ void JointImpedanceFrankaController::update(const ros::Time& /*time*/,
   for (size_t i = 0; i < 7; i++) {
     tau_d_calculated[i] = coriolis_factor_ * coriolis[i] +
                           k_gains_[i] * (q_filtered_[i] - q_[i]) +
-                          d_gains_[i] * (dq_d_[i] - dq_[i]);
+                          d_gains_[i] * (dq_d_[i] - dq_filtered_[i]);
                         }
   
 
